f0a0f9b4b7ebc666ae872e7067b8af6e35b3ff0a
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2014-07-14 14:23:08 +0200

    Update script to generate kaf from folia
    The script to generate kaf files from a folia xml file was updated. The
    following changes were made:
    - Use BeautifulSoup to parse the folia xml file, because the FoLiA XML
      parser in pynlpl does not yet accept sub-events (although these are
      part of FoLiA now)
    - Generate a kaf-file for each act in the FoLiA XML file (instead of
      one KAF file for each text). This was requested by the annotators.
    - KAF files are written directly to file and not printed to stdout
    - Added the terms layer in the KAF file
    Note: KAF expects sentence ids to be integers and not arbitrary strings.

--- folia2kaf.py
+++ folia2kaf.py
@@ -1,23 +1,104 @@
-"""Create KAF file based on FoLiA file
-Usage: python kaf2folia.py <file in>
+"""Create a KAF file for each act in a FoLiA file
+Usage: python folia2kaf.py <file in>
 """
 from pynlpl.formats import folia
 from lxml import etree
+from bs4 import BeautifulSoup
+from bs4_helpers import act, sentence, word, speaker_turn, note
+import os
 
-# Load document
-doc = folia.Document(file='medea-folia-no_events.xml')
 
-# output document
-root = etree.Element('KAF')
-kaf_document = etree.ElementTree(root)
-text = etree.SubElement(root, 'text')
+_folia_pos2kaf_pos = {
+    'WW': 'V',   # Werkwoord -> Verb
+    'VNW': 'Q',  # Voornaamwoord -> Pronoun
+    'VZ': 'P',   # Voorzetsel -> Preposition
+    'LID': 'D',  # Lidwoord -> Determiner
+    'N': 'N',    # Zelfstandig naamwoord -> Common noun
+    'VG': 'C',   # Voegwoord -> Conjunction
+    'TW': 'O',   # Interjection (?) -> Other
+    'BW': 'A',   # Bijwoord -> Adverb
+    'LET': 'O',  # Interpunctie -> Other
+    'TSW': 'O',  # Ja/nee(?) -> Other
+    'ADJ': 'G',  # Bijvoegelijk naamwoord -> Adjective
+    'SPEC': 'O'  # Special (niet nader gespecificeerd) -> Other
+    # Geen folia_pos voor Proper Noun (R)
+}
 
-# words
-for paragraph in doc.paragraphs():
-    for sentence in paragraph.sentences():
-        for word in sentence.words():
-            w = etree.SubElement(text, 'wf', wid=word.id, sent=sentence.id,
-                                 para=paragraph.id)
-            w.text = unicode(word)
 
-print etree.tostring(kaf_document, pretty_print=True)
+def kaf_file_name(input_file, act_number):
+    head, tail = os.path.split(input_file)
+    p = tail.split('.')   
+    return '{h}{s}{n}__act-0{a}.kaf'.format(a=act_number, 
+                                            n=p[0], 
+                                            h=head, 
+                                            s=os.sep)
+
+
+def add_word2kaf(elem, w_id, s_id, term_id, text, terms):
+    w_id = elem.get('xml:id')
+    w = etree.SubElement(text, 'wf', wid=w_id, sent=s_id)
+    if speaker_turn(elem):
+        w.text = unicode(elem['actor'])
+        lemma = unicode(elem['actor'])
+        pos = 'O'
+    else:
+        w.text = unicode(elem.t.string)
+        lemma = elem.lemma.get('class')
+        pos = _folia_pos2kaf_pos[elem.pos.get('head', 'SPEC')]
+
+    t_id = 't{wid}'.format(wid=term_id)
+    t = etree.SubElement(terms, 'term', tid=t_id, type='open', lemma=lemma, 
+                         pos=pos)
+    s = etree.SubElement(t, 'span')
+    target = etree.SubElement(s, 'target', id=w_id)
+
+
+if __name__ == '__main__':
+    #file_name = '/home/jvdzwaan/Documents/Emotion-mining/nederlab-voorbeeld/vos_002mede03_01.xml'
+    #file_name = '/home/jvdzwaan/Documents/Emotion-mining/nederlab-voorbeeld/feit007patr01_01.xml'
+    file_name = '/home/jvdzwaan/Documents/Emotion-mining/nederlab-voorbeeld/hoof002door01_01.xml'
+
+    # Load document
+    #doc = folia.Document(file='medea-folia-no_events.xml')
+    
+    with open(file_name) as f:
+        soup = BeautifulSoup(f, 'xml')
+
+    acts = soup.find_all(act)
+
+    act_number = 0
+    
+    s_id = 0  # in KAF, sentence numbers must be integers
+    w_id = None
+
+    for a in acts:
+        print 'act:', a.get('xml:id')
+        subacts = a.find_all(act)
+        if not subacts:
+            act_number += 1
+            term_id = 1
+
+            # create output kaf xml tree for act
+            root = etree.Element('KAF')
+            kaf_document = etree.ElementTree(root)
+            text = etree.SubElement(root, 'text')
+            terms = etree.SubElement(root, 'terms')
+
+            for elem in a.descendants:
+                if sentence(elem):
+                    s_id += 1
+                elif word(elem):
+                    add_word2kaf(elem, w_id, str(s_id), term_id, text, terms)
+
+                    term_id += 1
+                elif speaker_turn(elem):
+                    s_id += 1
+                    w_id = elem.get('xml:id')
+                    add_word2kaf(elem, w_id, str(s_id), term_id, text, terms)
+                    term_id += 1
+            
+            # write kaf xml tree to file
+            kaf_file = kaf_file_name(file_name, act_number)
+            print kaf_file
+            with open(kaf_file, 'w') as f:
+                kaf_document.write(f, xml_declaration=True, encoding='utf-8', method='xml', pretty_print=True)
