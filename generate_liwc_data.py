7f26d7715da9c052814c5334c268f385d3d21eea
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2014-07-30 14:08:28 +0200

    Add functionality for smoothing of raw liwc frequencies
    The raw frequencies of LIWC categories fluctuate a lot. In order to
    improve readability of the results, the raw frequencies are smoothed
    using a moving average.

--- generate_liwc_data.py
+++ generate_liwc_data.py
@@ -8,6 +8,7 @@ import argparse
 import codecs
 import os
 from collections import Counter
+import numpy as np
 
 from emotools.bs4_helpers import speaker_turn, sentence, entity
 
@@ -20,6 +21,14 @@ def get_characters(speakerturns):
         actor = turn['actor'].strip()
         characters[actor] += 1
     return characters
+
+
+def moving_average(a, n=3):
+    """Calculate the moving average of array a and window size n."""
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1:] / n
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -39,26 +48,33 @@ if __name__ == '__main__':
 
     character = 'Eloiza.'
 
-    print 'Sentence,{},{},{}'.format('affect', 'posemo', 'negemo')
+    #print 'Sentence,{},{},{}'.format('affect', 'posemo', 'negemo')
+    print 'Sentence,{},{}'.format('affect', 'smooth')
 
     sent = 0
     speaker = 'UNKNOWN'
     count_affect = 0
     count_posemo = 0
     count_negemo = 0
+    res_affect = np.array([])
+
     for elem in soup.descendants:
         if speaker_turn(elem):
             speaker = elem.get('actor').strip()
         elif sentence(elem):
-            if count_affect > 0 or count_posemo > 0 or count_negemo > 0:
-                if speaker == character:
-                    print '{},{},{},{}'.format(sent,
-                                               count_affect,
-                                               count_posemo,
-                                               count_negemo)
-                count_affect = 0
-                count_posemo = 0
-                count_negemo = 0
+            if speaker == character:
+                res_affect = np.append(res_affect, float(count_affect))
+                #print '{},{},{},{}'.format(sent,
+                #                           count_affect,
+                #                           count_posemo,
+                #                           count_negemo)
+            else: 
+                res_affect = np.append(res_affect, 0.0)
+            
+            count_affect = 0
+            count_posemo = 0
+            count_negemo = 0
+            
             sent += 1
         elif entity(elem) and elem.get('class').startswith('liwc-'):
             if elem.get('class') == 'liwc-Affect':
@@ -67,3 +83,21 @@ if __name__ == '__main__':
                 count_posemo += 1
             if elem.get('class') == 'liwc-Negemo':
                 count_negemo += 1
+
+    #print 'aantal zinnen:', sent, len(soup.find_all(sentence))
+    #print 'lengte res_affect array:', len(res_affect)
+    #print res_affect
+
+    s = 1
+    window = 5 
+    res_affect_smooth = moving_average(res_affect, window)
+    for i in range(len(res_affect)):
+        a = res_affect[i]
+        if i > window/2 and i < len(res_affect_smooth):
+            a_sm = res_affect_smooth[i-window/2]
+        else:
+            a_sm = 0.0
+
+        print '{},{},{}'.format(s, a, a_sm)
+        s += 1
+
