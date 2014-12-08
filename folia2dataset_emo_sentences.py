524449e2f58d41e9a03b04758f395d7a28dd9f17
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2014-12-08 13:58:53 +0100

    Update script to write output to file
    Updated the folia2dataset_emo_sentences script to accept an additional
    command line parameter specifying the directory where the output should
    be written to. The script should be used in the batch_do_python script.

--- folia2dataset_emo_sentences.py
+++ folia2dataset_emo_sentences.py
@@ -1,19 +1,29 @@
-"""Create a KAF file for each act in a FoLiA file
-Usage: python folia2kaf.py <file in> <output dir>
+"""Create data set to train emotional sentence classifier file
+The file contains (0=non-emotional, 1=emotional):
+<sentence>\t{0,1}
+
+Usage: python folia2dataset_emo_sentences.py <file in> <output dir>
+Or: ./batch_do_python.sh folia2dataset_emo_sentences.py <dir in> <output dir>
+(for a directory containing folia files)
 """
 from lxml import etree
 from bs4 import BeautifulSoup
 from emotools.bs4_helpers import sentence, note
 import argparse
+import codecs
+import os
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('file', help='the name of the FoLiA XML file that '
                         'should be processed.')
+    parser.add_argument('output_dir', help='the directory where the '
+                        'generated text files should be saved')
     args = parser.parse_args()
 
     file_name = args.file
+    output_dir = args.output_dir
 
     entity_class = u'EmbodiedEmotions'
 
@@ -26,45 +36,42 @@ if __name__ == '__main__':
     num_emotional = 0
     sents = set()
 
-    for event, elem in context:
-        if elem.get('class') == 'act':
-            # load act into memory
-            act_xml = BeautifulSoup(etree.tostring(elem), 'xml')
-            sentences = act_xml.find_all(sentence)
-            s = None
-            for sent in sentences:
-                if not note(sent.parent):
-                    num_sent += 1
-                    # some t elements appear to be empty (this is not allowed,
-                    # but it happens). So, check whether there is a string to
-                    # add before adding it.
-                    if sent.t:
-                        if sent.t.string:
-                            s = sent.t.string
+    out_file = os.path.join(output_dir, '{}.txt'.format(file_name[-20:-7]))
+    print 'Writing file: {}'.format(out_file)
+    with codecs.open(out_file, 'wb', encoding='utf-8') as f:
+        for event, elem in context:
+            if elem.get('class') == 'act':
+                # load act into memory
+                act_xml = BeautifulSoup(etree.tostring(elem), 'xml')
+                sentences = act_xml.find_all(sentence)
+                s = None
+                for sent in sentences:
+                    if not note(sent.parent):
+                        # some t elements appear to be empty (this is not allowed,
+                        # but it happens). So, check whether there is a string to
+                        # add before adding it.
+                        if sent.t:
+                            if sent.t.string:
+                                s = sent.t.string
 
-                    entities = sent.find_all('entity')
-                    emotional = False
-                    for entity in entities:
-                        if entity.attrs.get('class').startswith(entity_class):
-                            emotional = True
-                            break
-
-                    if emotional:
-                        num_emotional += 1
-
-                    # write sentence to file
-                    if s:
                         # remove duplicate sentences (mainly single word
                         # sentences indicating the speaker of a speaker turn,
                         # e.g., Medea.)
-                        #if emotional and (s, int(emotional)) in sents:
-                        #    print '-------------------->', s
-                        sents.add((s, int(emotional)))
+                        if s and s not in sents:
+                            sents.add(s)
+                            num_sent += 1
 
-    num_emotional_set = 0
-    for s, emotional in sents:
-        print '{}\t{}'.format(s.encode('utf-8'), emotional)
-        if emotional == 1:
-            num_emotional_set += 1
-    #print '#sentences in text: {}\n#emotional: {}'.format(num_sent, num_emotional)
-    #print '#sentences in set: {}\n#emotional: {}'.format(len(sents), num_emotional_set)
+                            entities = sent.find_all('entity')
+                            emotional = False
+                            for entity in entities:
+                                e = entity.attrs.get('class')
+                                if e.startswith(entity_class):
+                                    emotional = True
+                                    break
+
+                            if emotional:
+                                num_emotional += 1
+
+                            f.write(u'{}\t{}\n'.format(s, int(emotional)))
+
+    print '{} sentences, {} emotional\n'.format(num_sent, num_emotional)
