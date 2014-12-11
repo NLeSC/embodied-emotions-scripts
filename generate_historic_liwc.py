18b0d5bf6c8c444e49e12463d99f29f05109d493
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2014-12-11 19:06:09 +0100

    Update generate_historic_liwc script
    We found out that the INL lexicon service only returns spelling variants
    when the input is in lower case. Some of the words in the original Dutch
    LIWC dictionary are written with (a) capital(s). The script was updated
    to lowercase the terms send to the INL lexicon service.

--- generate_historic_liwc.py
+++ generate_historic_liwc.py
@@ -60,7 +60,8 @@ if __name__ == '__main__':
         # word
         else:
             entry = line.split()
-            term = entry[0]
+            # lexicon service needs lower case input
+            term = entry[0].lower()
             categories = entry[1:]
             sleep(0.3)
             words = get_spelling_variants(term, categories, 1600, 1830)
@@ -79,21 +80,21 @@ if __name__ == '__main__':
                     liwc_output[word] = categories
     #with codecs.open('liwc_output.json', 'w', 'utf8') as f:
     #    json.dump(liwc_output, f, sort_keys=True, ensure_ascii=False, indent=2)
-    
+
     #with codecs.open('liwc_output.json', 'rb', 'utf8') as f:
     #    liwc_output = json.load(f, encoding='utf-8')
-    
+
     # write spelling variants to file
     with codecs.open('liwc_spelling_variants.json', 'w', 'utf8') as f:
         json.dump(spelling_vars, f, sort_keys=True, ensure_ascii=False, indent=2)
 
-    #with codecs.open('historic_Dutch_LIWC.dic', 'wb', 'utf8') as f:
-    #    f.write('\n'.join(liwc_category_output))
-        
-    #    entries = liwc_output.keys()
-    #    entries.sort()
-    #    for entry in entries:
-    #        f.write(entry)
-    #        f.write('\t\t')
-    #        f.write('\t'.join(liwc_output[entry]))
-    #        f.write('\n')
+    with codecs.open('historic_Dutch_LIWC.dic', 'wb', 'utf8') as f:
+        f.write('\n'.join(liwc_category_output))
+
+        entries = liwc_output.keys()
+        entries.sort()
+        for entry in entries:
+            f.write(entry)
+            f.write('\t\t')
+            f.write('\t'.join(liwc_output[entry]))
+            f.write('\n')
