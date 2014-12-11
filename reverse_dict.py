dda57adf1b1ec7b6429f0891cf76bc4eb2001af9
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2014-12-11 10:57:59 +0100

    Save historic->modern dictionary to file
    The reversed dictionary is saved as a json object to file. The name of
    the file is the second command line argument of the script.

--- reverse_dict.py
+++ reverse_dict.py
@@ -10,9 +10,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('input_dict', help='the name of the json file '
                         'containing the modern->spelling variants dictionary')
+    parser.add_argument('output_dict', help='the name of the json file '
+                        'to save the historic->modern dictionary to')
     args = parser.parse_args()
 
     dict_file = args.input_dict
+    dict_out = args.output_dict
 
     modern_dict = {}
     historic_dict = {}
@@ -29,19 +32,27 @@ if __name__ == '__main__':
     print '#words in modern dict: {}'.format(len(modern_dict))
     print '#words in historic dict: {}'.format(len(historic_dict))
 
+    # save historic dictionary
+    with codecs.open(dict_out, 'wb', 'utf8') as f:
+        json.dump(historic_dict, f, sort_keys=True, ensure_ascii=False,
+                  indent=2)
+
     # find historic words that map to mulitple terms
     mappings_counter = Counter()
 
-    print '\nhistoric word\tmodern variant\tfrequency'
+    print '\nhistoric words that map to mulitple modern words'
+    print 'historic word\tmodern variant\tfrequency'
 
     for w, mappings in historic_dict.iteritems():
         mappings_counter[str(len(mappings)).zfill(3)] += 1
         if len(mappings) > 1:
             for variant, freq in mappings.iteritems():
-                print '{}\t{}\t{}'.format(w, variant, freq)
+                print '{}\t{}\t{}'.format(w.encode('utf-8'),
+                                          variant.encode('utf-8'),
+                                          freq)
 
     mp = mappings_counter.keys()
     mp.sort()
     print '\n#mappings\t#historic words'
     for m in mp:
-        print '{}\t{}'.format(m, mappings_counter[m])
+        print '{}\t{}'.format(m.encode('utf-8'), mappings_counter[m])
