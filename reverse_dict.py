8c011636c094eb979822eeed37294069dfc4cb00
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2015-01-05 13:27:44 +0100

    Update reverse_dict script
    Updated the reverse_dict script to not add modern->modern spelling
    variants in the hist2modern dictionaries it generates. This means that
    the hist2modern dictionaries are smaller and that spelling normalization
    can be done more efficiently.

--- reverse_dict.py
+++ reverse_dict.py
@@ -25,9 +25,10 @@ if __name__ == '__main__':
 
     for modern_word, variants in modern_dict.iteritems():
         for var in variants:
-            if var not in historic_dict.keys():
-                historic_dict[var] = Counter()
-            historic_dict[var][modern_word] += 1
+            if var != modern_word:
+                if var not in historic_dict.keys():
+                    historic_dict[var] = Counter()
+                historic_dict[var][modern_word] += 1
 
     print '#words in modern dict: {}'.format(len(modern_dict))
     print '#words in historic dict: {}'.format(len(historic_dict))
