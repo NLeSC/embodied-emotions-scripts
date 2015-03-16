975fc40b7055db0834713ffd91033bc8fac5d007
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2015-03-16 14:02:43 +0100

    Add heem label sets as importable variables
    Lists of HEEM labels have been added as variables that can be imported
    by other modules and used to print the labels in fixed orders.

---
+++ emotools/heem_utils.py
@@ -0,0 +1,24 @@
+heem_concept_type_labels = ['Emotie', 'Lichaamswerking', 'Lichaamsdeel',
+                            'EmotioneleHandeling']
+
+heem_emotion_labels = ['Achterdocht', 'Angst', 'Bedruktheid',
+                       'Beledigd', 'Berusting', 'Bezorgdheid', 'Blijdschap',
+                       'Eergevoel', 'Geluk', 'Gemis', 'Haat', 'Hebzucht',
+                       'Hoop', 'Jaloezie', 'Liefde', 'Mededogen', 'Ongeluk',
+                       'Ontroering', 'Ontzag', 'Opluchting', 'Overig',
+                       'Schaamte', 'Teleurstelling', 'Toewijding', 'Trots',
+                       'Trouw', 'Verdriet', 'Verlangen', 'Vertrouwen',
+                       'Verwondering', 'Walging', 'Wanhoop', 'Welwillendheid',
+                       'Woede', 'Wraakzucht', 'Wrevel', 'Wroeging', 'Wrok']
+
+heem_body_part_labels = ["Heart", "Longs", "Jaws", "Ears", "Mind", "Head",
+                         "Arms", "Hair", "Bones", "Lips", "Horns",
+                         "Throat", "Tooth", "Conscience", "Knees", "Limbs",
+                         "Breath", "Liver", "Phlegm", "Wound", "Fingers",
+                         "Snot", "Forehead", "Tears", "Legs", "Cheeks",
+                         "Body", "Eyes", "Senses", "Stomach", "Wrists",
+                         "Nails", "Mouth", "Breast", "Hands", "Marrow",
+                         "Varia", "Spirit", "Intestines", "Shoulder",
+                         "Feet", "Bile", "Voice", "Neck", "Skull", "Brain",
+                         "Soul", "Face", "Sweat", "Shins", "Veins",
+                         "Tongue", "Womb", "Blood"]
