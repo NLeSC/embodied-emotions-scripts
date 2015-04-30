91b020aeef0faa0682c0af8cc162b4f499d444c4
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2015-04-30 10:59:13 +0200

    Add script that adds delpher urls to list of edbo titles
    The input list is provided by Erik TKS.

---
+++ edbo_list2url.py
@@ -0,0 +1,25 @@
+"""Script to add delpher links to a list of edbo titles.
+
+Usage: python edbo_list2url.py <list with edbo titles and delpher ids>
+"""
+import argparse
+import pandas as pd
+#import os
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file_in', help='the name of the list containing the '
+                        'selected EDBO titles')
+    args = parser.parse_args()
+
+    file_name = args.file_in
+
+    titles = pd.read_csv(file_name, sep='\t', header=None,
+                         names=['ceneton_id', 'delpher_id', 'genre'])
+
+    url_template = 'http://www.delpher.nl/nl/boeken/view?identifier={}'
+    titles['url'] = pd.Series([url_template.format(id) for id in titles['delpher_id']])
+
+    #excel_name, _ = os.path.splitext(file_name)
+    #titles.to_excel(pd.ExcelWriter('{}.xlsx'.format(excel_name)))
+    titles.to_csv(file_name, sep='\t')
