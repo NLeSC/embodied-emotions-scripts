1d00bfe1353d23294437805d7faef52147ecd4fd
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2015-05-06 11:15:53 +0200

    Add script to collect ceneton texts from the ceneton website
    The script gathers the ceneton texts from a xlsx list and stores text
    files in a directory. The text files could use some postprocessing, it
    was not easy to separate the ceneton headers and footers from the texts.

---
+++ ceneton2txt.py
@@ -0,0 +1,49 @@
+"""Script to collect ceneton texts from the cebeton website.
+
+Usage: python ceneton2txt.py <ceneton list xml file> <output dir>
+"""
+import argparse
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import codecs
+import os
+import re
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file_in', help='the name of the list containing the '
+                        'selected ceneton titles (xlsx)')
+    parser.add_argument('dir_out', help='the name of the output directory')
+    args = parser.parse_args()
+
+    file_name = args.file_in
+    out_dir = args.dir_out
+
+    titles = pd.read_excel(file_name)
+    urls = titles['url'].str.startswith('http://')
+
+    for row in enumerate(titles[urls].values):
+        url = row[1][0]
+        m = re.search(r'/(\w+\d{4})\w?\.html', url)
+        if not m:
+            print url
+        else:
+            fname = m.group(1)
+            fname = fname.replace('.html', '')
+            out_text = os.path.join(out_dir, '{}.txt'.format(fname))
+
+            with codecs.open(out_text, 'wb', 'utf8') as f:
+                f.write(url+'\n')
+
+                result = requests.get(url)
+                if result.status_code != 200:
+                    f.write('Failed to get text\n')
+                else:
+                    # fix strange html entities
+                    txt = result.text.replace('&#146;', "'")
+                    txt = txt.replace('&#151;', "-")
+                    txt = txt.replace('&#148;', '"')
+                    soup = BeautifulSoup(txt)
+                    f.write(soup.get_text())
