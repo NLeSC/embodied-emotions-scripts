94a2caec42876fe0ef4c596497293125a7a1cef4
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2016-02-01 17:47:06 +0100

    Refactor naf2json script and add tests
    The naf2json script was refactored to use functions (which can be
    parallelized and tested). Also added tests for the new functions.

--- embem/kafnaftag/naf2json.py
+++ embem/kafnaftag/naf2json.py
@@ -64,6 +64,72 @@ def event_name(label, text_id):
     return '{}_{}'.format(label, text_id)
 
 
+def process_emotions(soup, text_id, year):
+    events = {}
+    mention_counter = Counter()
+
+    emotions = soup.find_all('emotion')
+    print 'text contains {} emotions'.format(len(emotions))
+    for emotion in emotions:
+        # for some reason, BeautifulSoup does not see the capitals in
+        # <externalRef>
+        emotion_labels = emotion.find_all('externalref')
+        ems = [l['reference'] for l in emotion_labels if l['reference'].startswith('emotionType:')]
+        for el in emotion_labels:
+            if el['resource'] == 'heem':
+                label = event_name(el['reference'], text_id)
+
+                if label not in events.keys():
+                    #print 'created new event', label
+                    events[label] = create_event(label, text_id, year)
+                m = create_mention(emotion, soup, text_id)
+                mention_counter[label] += 1
+                events[label]['mentions'].append(m)
+                events[label]['labels'].append(get_label(soup, m))
+
+        for el in emotion_labels:
+            #print 'checking for bodyparts'
+            if el['resource'] == 'heem:bodyParts':
+                print 'found bodypart', el['reference']
+                #print ems
+                for e in ems:
+                    events[event_name(e, text_id)]['actors'][el['reference']] = [el['reference']]
+                    #print 'event'
+                    #print events[e+text_id]
+                    #print 'actors'
+                    #print events[e+text_id]['actors']
+                    #print 'actor value'
+                    #print events[e+text_id]['actors'][el['reference']]
+                    #print
+
+    print 'found {} events and {} mentions'.format(len(mention_counter.keys()), sum(mention_counter.values()))
+    print 'top three events: {}'.format(' '.join(['{} ({})'.format(k, v) for k, v in mention_counter.most_common(3)]))
+    return events, mention_counter
+
+
+def get_num_sentences(soup):
+    return int((soup.find_all('wf')[-1]).get('sent'))
+
+
+def get_text(soup):
+    return ' '.join([wf.text for wf in soup.find_all('wf')])
+
+
+def add_source_text(soup, text_id, json_object):
+    json_object['timeline']['sources'].append({'uri': text_id,
+                                               'text': get_text(soup)})
+
+
+def add_events(events, num_sentences, json_object):
+    for event, data in events.iteritems():
+        data['climax'] = len(data['mentions'])+0.0/num_sentences*100
+
+        # TODO: make more intelligent choice for prefLabel (if possible)
+        # Also, it seems that prefLabel is not used in the visualization
+        data['prefLabel'] = [data['event']]
+        json_object['timeline']['events'].append(data)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('input_dir', help='directory containing naf XML files')
@@ -91,11 +157,10 @@ if __name__ == '__main__':
         }
     }
 
+    num_events = 0
+
     for i, fi in enumerate(xml_files):
         start = time.time()
-
-        events = {}
-        mention_counter = Counter()
 
         print '{} ({} of {})'.format(fi, (i + 1), len(xml_files))
         text_id = fi[-20:-7]
@@ -105,61 +170,24 @@ if __name__ == '__main__':
         with codecs.open(fi, 'rb', encoding='utf-8') as f:
             soup = BeautifulSoup(f, 'lxml')
 
-        num_sentences = int((soup.find_all('wf')[-1]).get('sent'))
+        year = text2year[text_id]
+        num_sentences = get_num_sentences(soup)
 
-        emotions = soup.find_all('emotion')
-        print 'text contains {} emotions'.format(len(emotions))
-        for emotion in emotions:
-            # for some reason, BeautifulSoup does not see the capitals in
-            # <externalRef>
-            emotion_labels = emotion.find_all('externalref')
-            ems = [l['reference'] for l in emotion_labels if l['reference'].startswith('emotionType:')]
-            for el in emotion_labels:
-                if el['resource'] == 'heem':
-                    label = event_name(el['reference'], text_id)
-
-                    if label not in events.keys():
-                        #print 'created new event', label
-                        year = text2year[text_id]
-                        events[label] = create_event(label, text_id, year)
-                    m = create_mention(emotion, soup, text_id)
-                    mention_counter[label] += 1
-                    events[label]['mentions'].append(m)
-                    events[label]['labels'].append(get_label(soup, m))
-
-            for el in emotion_labels:
-                #print 'checking for bodyparts'
-                if el['resource'] == 'heem:bodyParts':
-                    #print 'found bodypart', el['reference']
-                    #print ems
-                    for e in ems:
-                        events[event_name(e, text_id)]['actors'][el['reference']] = [el['reference']]
-                        #print 'event'
-                        #print events[e+text_id]
-                        #print 'actors'
-                        #print events[e+text_id]['actors']
-                        #print 'actor value'
-                        #print events[e+text_id]['actors'][el['reference']]
-                        #print
-
-        print 'found {} events and {} mentions'.format(len(mention_counter.keys()), sum(mention_counter.values()))
-        print 'top three events: {}'.format(' '.join(['{} ({})'.format(k, v) for k, v in mention_counter.most_common(3)]))
+        events, mention_counter = process_emotions(soup, text_id, year)
+        num_events += len(mention_counter.keys())
 
         end = time.time()
         print 'processing took {} sec.'.format(end-start)
 
-        text = ' '.join([wf.text for wf in soup.find_all('wf')])
-        json_out['timeline']['sources'].append({'uri': text_id, 'text': text})
+        add_events(events, num_sentences, json_out)
 
-        for event, data in events.iteritems():
-            data['climax'] = len(data['mentions'])+0.0/num_sentences*100
+        print 'Now {} events in the JSON'.format(len(json_out['timeline']['events']))
+        if num_events != len(json_out['timeline']['events']):
+            print 'Something is wrong with the event counts'
 
-            # TODO: make more intelligent choice for prefLabel (if possible)
-            # Also, it seems that prefLabel is not used in the visualization
-            data['prefLabel'] = [data['event']]
-            json_out['timeline']['events'].append(data)
+        add_source_text(soup, text_id, json_out)
 
-        # write temporary output
+        # write intermediary output
         temp_output_file = output_file.replace('.json', '{}.json'.format(i))
         print temp_output_file
         with codecs.open(temp_output_file, 'wb', encoding='utf-8') as f:
--- embem/tests/test_naf2json.py
+++ embem/tests/test_naf2json.py
@@ -3,7 +3,9 @@ from nose.tools import assert_equal
 from bs4 import BeautifulSoup
 from codecs import open
 
-from embem.kafnaftag.naf2json import create_mention, get_label, create_event
+from embem.kafnaftag.naf2json import create_mention, get_label, create_event, \
+    event_name, process_emotions, get_num_sentences, get_text, \
+    add_source_text, add_events
 
 
 def setup():
@@ -78,5 +80,70 @@ def test_create_event():
     assert_equal(event_object, create_event(emotion_label, text_id, year))
 
 
-#def test_process_emotions():
-#    process_emotions(emotions, soup, text_id, year)
+def test_event_name():
+    assert_equal('emotionType:joy_text_id', event_name('emotionType:joy', text_id))
+
+
+def test_process_emotions():
+    # check whether the events object and the mention_counter are reset for
+    # each text
+    year = 1719
+    events, mention_counter = process_emotions(soup, text_id, year)
+
+    yield assert_equal, 6, len(events)
+    yield assert_equal, 6, len(mention_counter)
+
+    year = 1718
+    events, mention_counter = process_emotions(soup, text_id+'2', year)
+
+    yield assert_equal, 6, len(events)
+    yield assert_equal, 6, len(mention_counter)
+
+
+def test_get_num_sentences():
+    assert_equal(12, get_num_sentences(soup))
+
+
+def test_get_text():
+    t = "Beslikte Swaantje , en Drooge Fobert ; of de boere rechtbank . Eerste bedryf . Eerste tooneel . Het Tooneel verbeeldeenig Geboomte , in 't verschiet een Heeren Huis . Crelis , en Kryn , malkander en op den weg ontmoetende . Crelis . Wat hebje veur met dat Cyteeren ? Wat meug jy leggen prossedeeren ? Myn goeje man ; 'k lag met jou plyt ; Ho , Kryn , je bent het byltje kwyt . Kryn . Wel , ouwe Cees , hoe keunje praeten ? 'k Zeg noch , ik zel 't ' er niet by laeten , Al zouw het onderst boven staen ; Lag jy vry uit ; 't zal zo niet gaen ."
+    assert_equal(t, get_text(soup))
+
+
+def test_add_source_text():
+    json_object = {
+        'timeline': {
+            'events': [],
+            'sources': []
+        }
+    }
+    add_source_text(soup, text_id, json_object)
+
+    r = {
+        'timeline': {
+            'events': [],
+            'sources': [{'uri': text_id, 'text': get_text(soup)}]
+        }
+    }
+
+    yield assert_equal, r, json_object
+
+
+def test_add_events():
+    # make sure that events are not added multiple time (keep track of counts)
+    year = 1918
+    json_object = {
+        'timeline': {
+            'events': [],
+            'sources': []
+        }
+    }
+    events, mention_counter = process_emotions(soup, text_id, year)
+    num_sentences = get_num_sentences(soup)
+
+    add_events(events, num_sentences, json_object)
+
+    yield assert_equal, 6, len(json_object['timeline']['events'])
+
+    add_events(events, num_sentences, json_object)
+
+    yield assert_equal, 12, len(json_object['timeline']['events'])
