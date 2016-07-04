b6cd07e1bac7fb2cd185903e7903fa532270556f
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2016-07-04 14:17:40 +0200

    Update script to merge all json files in a directory
    The compare_json script confirms that the result for the annotations
    is equivalent to what was generated before.

--- embem/kafnaftag/combine_json.py
+++ embem/kafnaftag/combine_json.py
@@ -1,13 +1,70 @@
 import json
 import click
+import glob
+import os
+
+from codecs import open
+
+from naf2json import merge_events
+
+
+def combine_events(result, to_merge):
+    events = {}
+    for e in to_merge['timeline']['events']:
+        events[e['event']] = e
+
+    # merge events that have the same id
+    new_events = []
+    for event in result['timeline']['events']:
+        event_id = event['event']
+        if event_id in events.keys():
+            new_events.append(merge_events(event, events[event_id]))
+            del events[event_id]
+        else:
+            new_events.append(event)
+
+    # add remaining events
+    for event, data in events.iteritems():
+        new_events.append(data)
+
+    result['timeline']['events'] = new_events
+
+    return result
 
 
 @click.command()
-@click.argument('input_file', type=click.File())
+@click.argument('input_dir', type=click.Path(exists=True))
 @click.argument('output_file', type=click.Path())
-def cli(input_file, output_file):
-    data = json.load(input_file)
-    print len(data['timeline']['events'])
+def cli(input_dir, output_file):
+    output_dir = os.path.dirname(click.format_filename(output_file))
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    json_files = glob.glob('{}/*.json'.format(input_dir))
+    print('Found {} json files'.format(len(json_files)))
+
+    data = None
+
+    for i, fi in enumerate(json_files):
+        print '{} ({} of {})'.format(fi, (i + 1), len(json_files))
+        text_id = os.path.basename(fi).split('.')[0].replace('.json', '')
+        print text_id
+        with open(fi, 'r', encoding='utf-8') as f:
+            t = json.load(f)
+
+        if not data:
+            data = t
+        else:
+            # merge events
+            data = combine_events(data, t)
+
+            # merge source texts
+            data['timeline']['sources'].append(t['timeline']['sources'][0])
+
+    with open(output_file, 'wb', encoding='utf-8') as f:
+        json.dump(data, f, sort_keys=True, indent=4)
+
 
 if __name__ == '__main__':
     cli()
